bc30584448ef18d9317e472dd1a2a362ce07ac10
devesh verma <deveshsingh9756@gmail.com>
2025-03-24 14:43:19 +0530

    added modify cart api

--- src/routes/cartRoute.js
+++ src/routes/cartRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
  
- const {  getCartByUser } = require('../controllers/cartController.js');
+ const {  getCartByUser, modifyProductTocart } = require('../controllers/cartController.js');
  const { isLoggedIn } = require('../validation/authValidator.js');
  
  const cartRouter = express.Router();
  
- 
  cartRouter.get('/', isLoggedIn, getCartByUser);
+ 
+ cartRouter.post('/:operation/:productId', isLoggedIn, modifyProductTocart);
  
  module.exports = cartRouter;
